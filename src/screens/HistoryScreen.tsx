5920544993c0aaeceac0d4eb02392ed3fd131a74
kishlay <kishlay141@gmail.com>
2025-06-14 16:32:54 +0530

    modified HistoryScreen.tsx

--- src/screens/HistoryScreen.tsx
+++ src/screens/HistoryScreen.tsx
@@ -1,2 +1,15 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+
+const HistoryScreen = () => (
+  <View style={styles.container}>
+    <Text style={styles.text}>QR History</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: "center", justifyContent: "center" },
+  text: { fontSize: 22, fontWeight: "bold" },
+});
+
+export default HistoryScreen;
