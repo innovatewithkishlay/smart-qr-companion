840d5aa73828363a25a958b98f9879f0d8db3bf8
kishlay <kishlay141@gmail.com>
2025-06-14 16:30:31 +0530

    modified the GenerateScreen

--- src/screens/GenerateScreen.tsx
+++ src/screens/GenerateScreen.tsx
@@ -1,2 +1,15 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+
+const GenerateScreen = () => (
+  <View style={styles.container}>
+    <Text style={styles.text}>Generate QR Code</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: "center", justifyContent: "center" },
+  text: { fontSize: 22, fontWeight: "bold" },
+});
+
+export default GenerateScreen;
