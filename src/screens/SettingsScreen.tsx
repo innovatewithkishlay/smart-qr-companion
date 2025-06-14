2561fada901be571748c40c40c1cd559cd131dd6
kishlay <kishlay141@gmail.com>
2025-06-14 16:34:09 +0530

    modified the SettingsScreen.tsx

--- src/screens/SettingsScreen.tsx
+++ src/screens/SettingsScreen.tsx
@@ -1,2 +1,15 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+
+const SettingsScreen = () => (
+  <View style={styles.container}>
+    <Text style={styles.text}>Settings</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: { flex: 1, alignItems: "center", justifyContent: "center" },
+  text: { fontSize: 22, fontWeight: "bold" },
+});
+
+export default SettingsScreen;
