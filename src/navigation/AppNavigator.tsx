795d7d3c576762b4ff39dc1d81094b4b26b0a622
kishlay <kishlay141@gmail.com>
2025-06-14 16:35:26 +0530

    imported files

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -2,3 +2,6 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import GenerateScreen from "../screens/GenerateScreen";
+import ScanScreen from "../screens/ScanScreen";
+import HistoryScreen from "../screens/HistoryScreen";
+import SettingsScreen from "../screens/SettingsScreen";
