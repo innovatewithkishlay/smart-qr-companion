8dfd929c3137ce450b3810f95db18f9f7b349a43
kishlay <kishlay141@gmail.com>
2025-06-15 07:28:16 +0530

    updated the App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import OnboardingScreen from "./screens/OnboardingScreen";
 import AppNavigator from "./src/navigation/AppNavigator";
 
-export default function App() {
-  return <AppNavigator />;
-}
+const App = () => {
+  const [showOnboarding, setShowOnboarding] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      const hasLaunched = await AsyncStorage.getItem("hasLaunched");
+      setShowOnboarding(!hasLaunched);
+    })();
+  }, []);
+
+  const handleOnboardingDone = async () => {
+    await AsyncStorage.setItem("hasLaunched", "true");
+    setShowOnboarding(false);
+  };
+
+  if (showOnboarding === null) return null;
+
+  return showOnboarding ? (
+    <OnboardingScreen onDone={handleOnboardingDone} />
+  ) : (
+    <AppNavigator />
+  );
+};
+
+export default App;
